44cb3a1c7d0f2ffb0deb5e490d94c1246fb735b2
William Mckee <williamcmckee@protonmail.com>
2024-09-11 16:31:12 +1000

    allow from all subdomains images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    images: {
+        domains: ['*.public.blob.vercel-storage.com'],
+      },
+};
 
 export default config;
